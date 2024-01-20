53ec8fd18d9e454537bf0e59b7975949a237fbf8
SeongmoAhn <asm0619@naver.com>
2024-01-20 12:38:25 +0000

    init 파일

---
+++ compile/init.h
@@ -0,0 +1,21 @@
+#ifndef __INIT_H__
+#define __INIT_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
+#define PATH_MAXLEN 128
+
+char compile_directory[PATH_MAXLEN];
+
+char lang_type[] = {"c", "cpp", "java", "py"};
+
+void init(void) {
+    getcwd(compile_directory, PATH_MAXLEN);
+}
+
+#endif // __INIT_H__
