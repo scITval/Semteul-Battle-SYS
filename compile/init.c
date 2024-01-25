31ded9f17cd88a721d46690e0865437e98e43733
SeongmoAhn <asm0619@naver.com>
2024-01-25 12:16:47 +0000

    ParseInitFile() 주석 처리

--- compile/init.c
+++ compile/init.c
@@ -19,9 +19,11 @@ char *GetCompilePath(void) { return compilePath; }
 char *GetTestPath(void) { return testPath; }
 char *GetResultPath(void) { return resultPath; }
 
+// compile/code/init.txt 파일에서 정보들을 정리하는 함수
+// 성공 시 0, 실패 시 1을 반환
 int ParseInitFile(void) {
-    int fd_init;
-    char init_txt_path[PATH_MAXLEN];
+    int fd_init; // init.txt 파일 디스크립터
+    char init_txt_path[PATH_MAXLEN]; // init.txt 절대 경로
     sprintf(init_txt_path, "%s/code/init.txt", GetCompilePath());
     if ((fd_init = open(init_txt_path, O_RDONLY)) < 0) {
         fprintf(stderr, "open error for %s\n", init_txt_path);
@@ -34,6 +36,7 @@ int ParseInitFile(void) {
     ssize_t len;
     int idx = 0, cnt = 0;
     int info[5];
+    // 개행 문자를 기준으로 5줄은 정수 값으로 입력받은 후 각 정보를 grade.c에서 사용할 변수에 저장
     while ((len = read(fd_init, temp, 1)) > 0) {
         if (temp[0] == '\n') {
             buf[idx] = 0;
@@ -53,9 +56,10 @@ int ParseInitFile(void) {
     timeLimit = info[3];
     memLimit = info[4];
     
-    int fd_source;
-    char source_path[PATH_MAXLEN];
+    int fd_source; // source.파일형식 파일 디스크립터
+    char source_path[PATH_MAXLEN]; // source.파일형식 절대 경로
     sprintf(source_path, "%s/code/source.%s", GetCompilePath(), languageType[language]);
+    // source.파일형식 소스파일을 644권한으로 생성
     if ((fd_source = open(source_path, O_WRONLY | O_CREAT | O_TRUNC, 0644)) < 0) {
         fprintf(stderr, "create error for %s\n", source_path);
         close(fd_init);
@@ -63,6 +67,7 @@ int ParseInitFile(void) {
         return 1;
     }
 
+    // init.txt에 남은 부분(코드)을 source.파일형식 소스파일에 복사
     while ((len = read(fd_init, buf, BUFFER_SIZE)) > 0) {
         if (len < 0) {
             fprintf(stderr, "read error for %s\n", init_txt_path);
